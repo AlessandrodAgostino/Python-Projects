6d6abbd0b7db18d1705e6114ed9642753f4aa6fc
AlessandrodAgostino <alessandro.dagostino96@gmail.com>
2019-06-09 10:43:04 +0200

    files ready for sending

--- Brain Challenge/Pipeline_GridschearchCV.py
+++ Brain Challenge/Pipeline_GridschearchCV.py
@@ -1,57 +1,99 @@
-# Authors: Robert McGibbon, Joel Nothman, Guillaume Lemaitre
-
-
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.datasets import load_digits
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import FeatureUnion, Pipeline
+from dataclasses import dataclass
+from sklearn.preprocessing import MinMaxScaler
+from os.path import join as pj
+from sklearn.linear_model import RidgeCV
+from sklearn.linear_model import Ridge
+from sklearn.gaussian_process.kernels import RBF, RationalQuadratic, Matern, DotProduct, WhiteKernel
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.model_selection import train_test_split as tts
 from sklearn.model_selection import GridSearchCV
-from sklearn.pipeline import Pipeline
-from sklearn.svm import LinearSVC
-from sklearn.decomposition import PCA, NMF
-from sklearn.feature_selection import SelectKBest, chi2
 
-print(__doc__)
+#Definition of my custom Tranformer
+class FilterRidgeCoefficients(BaseEstimator, TransformerMixin):
+    """docstring for FilterRidgeCoefficients
+        This custom transformer filter those features with a weight greater than a certain treshold."""
+    def __init__(self, ridge_coefs, treshold):
+        #previuous ridge regression's coefficient array
+        self.ridge_coefs = ridge_coefs
+        #treshold for filtering
+        self.treshold = treshold
 
-pipe = Pipeline([('reduce_dim', 'passthrough'),('classify', LinearSVC(dual=False, max_iter=10000))])
+    def fit( self, X, y = None ):
+        return self
 
-N_FEATURES_OPTIONS = [2, 4, 8]
-C_OPTIONS = [1, 10, 100, 1000]
-param_grid = [
-    {
-        'reduce_dim': [PCA(iterated_power=7), NMF()],
-        'reduce_dim__n_components': N_FEATURES_OPTIONS,
-        'classify__C': C_OPTIONS
-    },
-    {
-        'reduce_dim': [SelectKBest(chi2)],
-        'reduce_dim__k': N_FEATURES_OPTIONS,
-        'classify__C': C_OPTIONS
-    },
-]
-reducer_labels = ['PCA', 'NMF', 'KBest(chi2)']
+    def transform( self, X, y = None):
+        filter = ridge_coefs > self.treshold
+        return X[:,filter]
 
-grid = GridSearchCV(pipe, cv=5, n_jobs=1, param_grid=param_grid, iid=False)
-digits = load_digits()
-grid.fit(digits.data, digits.target)
+def main():
+    #Remote path for Data and Results directories
+    data_dir='/home/STUDENTI/alessandr.dagostino2/Python-Projects/Brain Challenge/Data'
+    results_dir='/home/STUDENTI/alessandr.dagostino2/Python-Projects/Brain Challenge/Results'
 
-mean_scores = np.array(grid.cv_results_['mean_test_score'])
-# scores are in the order of param_grid iteration, which is alphabetical
-mean_scores = mean_scores.reshape(len(C_OPTIONS), -1, len(N_FEATURES_OPTIONS))
-# select score for best C
-mean_scores = mean_scores.max(axis=0)
-bar_offsets = (np.arange(len(N_FEATURES_OPTIONS)) *
-               (len(reducer_labels) + 1) + .5)
+    #Loading the data as a pd.DataFrame
+    data_train = pd.read_csv(pj(data_dir, 'Training_Set_YESregressBYeTIVifCorr_LogScaled_combat_SVA.txt'),
+                            header=0, sep='\t')
+    #Extracting target variable as a np.array
+    y = data_train['age_floor'].values
+    #Extracting interesting features as a np.array
+    feats= data_train.loc[:,'lh_bankssts_area' :'rh.Whole_hippocampus'].values
+    #Shape of data:
+    y.shape
+    feats.shape
 
-plt.figure()
-COLORS = 'bgrcmyk'
-for i, (label, reducer_scores) in enumerate(zip(reducer_labels, mean_scores)):
-    plt.bar(bar_offsets + i, reducer_scores, label=label, color=COLORS[i])
+    #Scaling of data according to the MinMaxScaler
+    train_feats=MinMaxScaler().fit_transform(feats)
+    #Range of values where to seek the best alpha
+    alphas=np.arange(0.001, 10, 0.005)
+    ridge=RidgeCV(alphas=alphas, fit_intercept=True)
+    #Finding the best penalization parameter alpha
+    ridge.fit(train_feats, y)
+    alpha = ridge.alpha_
+    alpha
+    #Saving all the coefficients
+    ridge_coefs = np.sort(np.abs(ridge.coef_))
+    ridge_coefs.shape
+    #Score of the best ridge regression:
+    ridge.score(train_feats,y)
 
-plt.title("Comparing feature reduction techniques")
-plt.xlabel('Reduced number of features')
-plt.xticks(bar_offsets + len(reducer_labels) / 2, N_FEATURES_OPTIONS)
-plt.ylabel('Digit classification accuracy')
-plt.ylim((0, 1))
-plt.legend(loc='upper left')
+    #Defining all the tresholds where to filter coefficients
+    tresholds = np.linspace(np.min(ridge_coefs), np.max(ridge_coefs)/4, num=10)
 
-plt.show()
+    #Definition of:
+    #the filter tranformer
+    filter_ridge_coef = FilterRidgeCoefficients(ridge_coefs, 0)
+    #the GPR
+    gaussian_process = GaussianProcessRegressor(normalize_y=True, n_restarts_optimizer=50, kernel=RBF())
+    #the pipeline that merge them
+    pipe_filter_gpr = Pipeline([('filter', filter_ridge_coef), ('GPR', gaussian_process)])
+    #the parameter grid with different tresholds and different kernels to try
+    parameter_grid = {'filter__treshold': tresholds,
+                      'GPR__kernel': [RBF(), DotProduct() + WhiteKernel()]}
+    #Gridscearch of the pipeline on those parameters
+    grid = GridSearchCV(pipe_filter_gpr, param_grid=parameter_grid, cv=5)
+
+    #Splitting the data:
+    x_train,x_test,y_train,y_test=tts(feats, y, test_size=0.1, shuffle=False)
+
+    #Scearching for the best estimator:
+    grid.fit(x_train,y_train)
+
+    with open(pj(results_dir,'writing_test.txt'), 'w') as the_file:
+        the_file.write('Result of the run of:\n')
+        the_file.write(str(grid))
+        the_file.write('\n\nBest parameters were:\n')
+        the_file.write(str(grid.best_params_))
+        the_file.write('\n\nBest score was\n')
+        the_file.write(str(grid.best_score_))
+        the_file.write('\n\nPrediction on the test set is:\n')
+        the_file.write(str(grid.score(x_test,y_test)))
+        the_file.write('\n')
+
+if __name__ == '__main__':
+    main()
+
+# %%
