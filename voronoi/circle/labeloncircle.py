ca44a772bb1f1065bb753424e4e7bb3f21860334
AlessandrodAgostino <alessandro.dagostino96@gmail.com>
2019-06-24 12:13:30 +0200

    voronoi advancing

--- voronoi/circle/labeloncircle.py
+++ voronoi/circle/labeloncircle.py
@@ -10,47 +10,50 @@ from scipy.spatial import Voronoi
 from SALib.sample import saltelli
 from skimage import io
 
-
+#First circle and interior
 thetas = np.linspace(0,2*np.pi,15)
 r = 1
 c = (2,3)
 x = np.cos(thetas)*r + c[0]
 y = np.sin(thetas)*r + c[1]
 circle_point = np.stack((x,y)).T
-circle = Polygon(circle_point)
+circle1 = Polygon(circle_point)
 
-thetas = np.linspace(0,2*np.pi,15)
 r = 0.5
 c = (2,3)
 x = np.cos(thetas)*r + c[0]
 y = np.sin(thetas)*r + c[1]
 internal_point = np.stack((x,y)).T
-internal = Polygon(internal_point)
+internal1 = Polygon(internal_point)
 
+#Second circle and interior
+r = 1
+c = (7,8)
+x = np.cos(thetas)*r + c[0]
+y = np.sin(thetas)*r + c[1]
+circle_point = np.stack((x,y)).T
+circle2 = Polygon(circle_point)
 
+r = 0.5
+c = (7,8)
+x = np.cos(thetas)*r + c[0]
+y = np.sin(thetas)*r + c[1]
+internal_point = np.stack((x,y)).T
+internal2 = Polygon(internal_point)
 
+#Plotting
 fig = plt.figure()
-plt.fill(*circle.exterior.xy, 'k', alpha = 0.5)
-plt.fill(*internal.exterior.xy, 'k')
+plt.fill(*circle1.exterior.xy, 'k', alpha = 0.5)
+plt.fill(*internal1.exterior.xy, 'k')
 
-plt.ylim(0,5)
-plt.xlim(0,5)
+plt.fill(*circle2.exterior.xy, 'k', alpha = 0.5)
+plt.fill(*internal2.exterior.xy, 'k')
+
+
+plt.ylim(0,10)
+plt.xlim(0,10)
 plt.axis('off')
 fig.savefig('small_black_circle.png', bbox_inches='tight',dpi=10)
-
-#%%
-from scipy.ndimage import label, generate_binary_structure
-a = np.array([[0,0,0,0,0,0,0,0],
-              [0,0,0,1,1,0,0,0],
-              [0,0,2,0,0,1,0,0],
-              [0,0,1,0,0,1,0,0],
-              [0,0,0,1,3,0,0,0],
-              [0,0,0,0,0,0,0,0]])
-s = [[1,1,1],
-     [1,1,1],
-     [1,1,1]]
-labeled_array, num_features = label(a, structure = s)
-labeled_array
 
 #%%
 image = io.imread('small_black_circle.png')
@@ -70,6 +73,7 @@ v_select_dark = np.vectorize(select_dark)
 sel_dark = v_select_dark(image)
 
 core, _ = label(sel_dark, structure = s)
+np.unique(core)
 io.imsave('core.png',core*255)
 #%%
 def select_grey(pix):
