e5e3a8fa1accb4733d4aa0d4ef18b105c74b947e
AlessandrodAgostino <alessandro.dagostino96@gmail.com>
2019-06-24 10:33:32 +0200

    again

--- voronoi/labeloncircle.py
+++ voronoi/labeloncircle.py
@@ -37,6 +37,21 @@ plt.ylim(0,5)
 plt.xlim(0,5)
 plt.axis('off')
 fig.savefig('small_black_circle.png', bbox_inches='tight',dpi=10)
+
+#%%
+from scipy.ndimage import label, generate_binary_structure
+a = np.array([[0,0,0,0,0,0,0,0],
+              [0,0,0,1,1,0,0,0],
+              [0,0,1,0,0,1,0,0],
+              [0,0,1,0,0,1,0,0],
+              [0,0,0,1,1,0,0,0],
+              [0,0,0,0,0,0,0,0]])
+s = [[1,1,1],
+     [1,1,1],
+     [1,1,1]]
+labeled_array, num_features = label(a, structure = s)
+labeled_array
+
 #%%
 selection = io.imread('small_black_circle.png')
 selection = selection[:,:,0]
@@ -44,16 +59,19 @@ selection.shape
 selection.size
 plt.hist(selection)
 
-selection > 150
+def tresh_bin(lev):
+    if lev >= 60 and lex <=110: return 1
+    return 0
 
 
+vec_tresh_lev = np.vectorize(tresh_lev)
 
-selection[selection > 150]=255
-selection[selection > 90]=128
-selection[selection < 90]=0
-np.unique(selection)
-plt.hist(selection)
+dark_sel = vec_tresh_lev(selection)
+
+np.unique(dark_sel)
+plt.hist(dark_sel)
 
 labeled_image, num_features = label(selection)
 num_features
 np.unique(labeled_image)
+labeled_image
